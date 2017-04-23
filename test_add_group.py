fb69d2f72baf1f536a7a391a9212b4582905cf2c
Olena Piankova <el.piankova@gmail.com>
2017-04-23 19:15:17 +0300

    Добавлены комментарии, которые характеризуют наши действия в тесте

--- test_add_group.py
+++ test_add_group.py
@@ -9,7 +9,9 @@ class test_add_group(unittest.TestCase):
     
     def test_test_add_group(self):
         wd = self.wd
+        # Open home page
         wd.get("http://localhost:8888/addressbook/")
+        # Login
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
@@ -17,22 +19,25 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+        # Open group page
         wd.find_element_by_xpath('//*[@id="nav"]/ul/li[3]/a').click()
+        # Init group creation
         wd.find_element_by_name("new").click()
+        # Fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("group")
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("groupe_name")
+        wd.find_element_by_name("group_name").send_keys("group_name")
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
         wd.find_element_by_name("group_header").send_keys("header")
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys("footer")
+        # Submit group
         wd.find_element_by_name("submit").click()
+        # Return to group page
         wd.find_element_by_link_text("group page").click()
+        # Logout
         wd.find_element_by_css_selector('form[name="logout"] > a').click()
     
     def tearDown(self):
