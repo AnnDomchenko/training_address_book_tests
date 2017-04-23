ca6fd68f7af20af2bcc324e694f02ac39ff6bd0f
Olena Piankova <el.piankova@gmail.com>
2017-04-23 20:02:59 +0300

    Изменение scope="session" для pytest.fixture для того, чтоб использовать один запуск браузера для всех тестов

--- conftest.py
+++ conftest.py
@@ -2,7 +2,7 @@ import pytest
 from web_api.addressbook_api import AddressBookAPI
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def app():
     addr_api = AddressBookAPI()
     addr_api.login(username="admin", password="secret")
