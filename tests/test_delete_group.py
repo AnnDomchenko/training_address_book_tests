f95fcc53c8ded9c910463849b40ab4fb5339846a
Olena Piankova <el.piankova@gmail.com>
2017-04-23 20:26:29 +0300

    Выделили фикстуру, которыя проверяет, если ли созданные группы, если нет создает группу

--- tests/test_delete_group.py
+++ tests/test_delete_group.py
@@ -1,4 +1,15 @@
-def test_delete_group(app, init_login):
+import pytest
+from models.group import Group
+
+
+@pytest.fixture
+def init_group(app, init_login):
+    if not app.is_group_present():
+        test_group = Group(name="test name")
+        app.create_group(test_group)
+
+
+def test_delete_group(app, init_login, init_group):
     app.open_group_page()
     app.delete_group_by_number(0)
     assert "Group has been removed." in app.find_message()
--- web_api/addressbook_api.py
+++ web_api/addressbook_api.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
 
 class AddressBookAPI:
@@ -23,6 +25,18 @@ class AddressBookAPI:
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
 
+    def is_element_present(self, by, locator):
+        wd = self.wd
+        try:
+            wd.find_element(by, locator)
+            return True
+        except NoSuchElementException:
+            return False
+
+    def is_group_present(self):
+        self.open_group_page()
+        self.is_element_present(By.NAME, "selected[]")
+
     def open_group_page(self):
         wd = self.wd
         # Open group page
@@ -33,15 +47,18 @@ class AddressBookAPI:
         # Init group creation
         wd.find_element_by_name("new").click()
         # Fill group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        if group.name is not None:
+            wd.find_element_by_name("group_name").click()
+            wd.find_element_by_name("group_name").clear()
+            wd.find_element_by_name("group_name").send_keys(group.name)
+        if group.header is not None:
+            wd.find_element_by_name("group_header").click()
+            wd.find_element_by_name("group_header").clear()
+            wd.find_element_by_name("group_header").send_keys(group.header)
+        if group.footer is not None:
+            wd.find_element_by_name("group_footer").click()
+            wd.find_element_by_name("group_footer").clear()
+            wd.find_element_by_name("group_footer").send_keys(group.footer)
         # Submit group
         wd.find_element_by_name("submit").click()
 
