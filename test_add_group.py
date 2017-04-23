9db355f0f55effd933d43eeeadf319e247cb7b21
Olena Piankova <el.piankova@gmail.com>
2017-04-23 19:07:40 +0300

    Исправнен код, чтоб был независим от локализации (языка). Удален лишний код

--- test_add_group.py
+++ test_add_group.py
@@ -1,22 +1,13 @@
-# -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.common.action_chains import ActionChains
-import time, unittest
+from selenium import webdriver
+import unittest
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
 
 class test_add_group(unittest.TestCase):
     def setUp(self):
-        self.wd = WebDriver()
+        self.wd = webdriver.Chrome()
         self.wd.implicitly_wait(60)
     
     def test_test_add_group(self):
-        success = True
         wd = self.wd
         wd.get("http://localhost:8888/addressbook/")
         wd.find_element_by_name("user").click()
@@ -26,7 +17,7 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-        wd.find_element_by_link_text("Группы").click()
+        wd.find_element_by_xpath('//*[@id="nav"]/ul/li[3]/a').click()
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
@@ -34,8 +25,6 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys("groupe_name")
-        if not wd.find_element_by_xpath("//div[@id='content']//select[normalize-space(.)='[none]']//option[1]").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']//select[normalize-space(.)='[none]']//option[1]").click()
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
         wd.find_element_by_name("group_header").send_keys("header")
@@ -44,8 +33,7 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_name("group_footer").send_keys("footer")
         wd.find_element_by_name("submit").click()
         wd.find_element_by_link_text("group page").click()
-        wd.find_element_by_link_text("Выйти").click()
-        self.assertTrue(success)
+        wd.find_element_by_css_selector('form[name="logout"] > a').click()
     
     def tearDown(self):
         self.wd.quit()
