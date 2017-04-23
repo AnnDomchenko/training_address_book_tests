a1b925ed704e541e48fff574921a96ad0c773fe6
Olena Piankova <el.piankova@gmail.com>
2017-04-23 19:34:57 +0300

    Выделено высокоуровневое API взаимодействия с приложением Addressbook

--- test_add_group.py
+++ test_add_group.py
@@ -1,72 +1,23 @@
-from selenium import webdriver
 import unittest
 from models.group import Group
+from web_api.addressbook_api import AddressBookAPI
 
 
 class test_add_group(unittest.TestCase):
     def setUp(self):
-        self.wd = webdriver.Chrome()
-        self.wd.implicitly_wait(60)
+        self.app = AddressBookAPI()
     
     def test_test_add_group(self):
         test_group = Group(name="group_name", header="header", footer="footer")
-        self.open_home_page()
-        self.login(username="admin", password="secret")
-        self.open_group_page()
-        self.create_group(test_group)
-        self.return_to_group_page()
-        self.logout()
-
-    def open_home_page(self):
-        wd = self.wd
-        # Open home page
-        wd.get("http://localhost:8888/addressbook/")
-
-    def login(self, username, password):
-        wd = self.wd
-        # Login
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-
-    def open_group_page(self):
-        wd = self.wd
-        # Open group page
-        wd.find_element_by_xpath('//*[@id="nav"]/ul/li[3]/a').click()
-
-    def create_group(self, group):
-        wd = self.wd
-        # Init group creation
-        wd.find_element_by_name("new").click()
-        # Fill group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # Submit group
-        wd.find_element_by_name("submit").click()
-
-    def return_to_group_page(self):
-        wd = self.wd
-        # Return to group page
-        wd.find_element_by_link_text("group page").click()
-
-    def logout(self):
-        wd = self.wd
-        # Logout
-        wd.find_element_by_css_selector('form[name="logout"] > a').click()
+        self.app.open_home_page()
+        self.app.login(username="admin", password="secret")
+        self.app.open_group_page()
+        self.app.create_group(test_group)
+        self.app.return_to_group_page()
+        self.app.logout()
 
     def tearDown(self):
-        self.wd.quit()
+        self.app.destroy()
 
 if __name__ == '__main__':
     unittest.main()
---
+++ web_api/addressbook_api.py
@@ -0,0 +1,58 @@
+from selenium import webdriver
+
+
+class AddressBookAPI:
+    def __init__(self):
+        self.wd = webdriver.Chrome()
+        self.wd.implicitly_wait(5)
+
+    def open_home_page(self):
+        wd = self.wd
+        # Open home page
+        wd.get("http://localhost:8888/addressbook/")
+
+    def login(self, username, password):
+        wd = self.wd
+        # Login
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+
+    def open_group_page(self):
+        wd = self.wd
+        # Open group page
+        wd.find_element_by_xpath('//*[@id="nav"]/ul/li[3]/a').click()
+
+    def create_group(self, group):
+        wd = self.wd
+        # Init group creation
+        wd.find_element_by_name("new").click()
+        # Fill group form
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        # Submit group
+        wd.find_element_by_name("submit").click()
+
+    def return_to_group_page(self):
+        wd = self.wd
+        # Return to group page
+        wd.find_element_by_link_text("group page").click()
+
+    def logout(self):
+        wd = self.wd
+        # Logout
+        wd.find_element_by_css_selector('form[name="logout"] > a').click()
+
+    def destroy(self):
+        self.wd.quit()
