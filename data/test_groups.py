e54e8a3db3d1a78f7f9160b3e1516cc9f7139a2b
Olena Piankova <el.piankova@gmail.com>
2017-04-30 14:54:26 +0300

    Unicode символы для генерации рандомных строк

--- data/test_groups.py
+++ data/test_groups.py
@@ -2,10 +2,14 @@ import random
 import string
 from models.group import Group
 
+utf_symbols = ''.join([chr(l) for l in range(1, 0x10ffff) if chr(l).isprintable()])
+cyr_symbol = ''.join([chr(l) for l in range(0x0400, 0x04FF) if chr(l).isprintable()])
+cyr_symbol_ru_uk = ''.join([chr(l) for l in range(0x0410, 0x0457) if chr(l).isprintable()])
+
 
 def random_string(maxlen):
     length = random.randrange(maxlen)
-    symbols = string.ascii_letters + string.digits + " "*10 + string.punctuation
+    symbols = string.ascii_letters + string.digits + " "*10 + string.punctuation + cyr_symbol_ru_uk
     return ''.join([random.choice(symbols) for _ in range(length)])
 
 
