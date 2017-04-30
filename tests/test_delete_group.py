b79f53bbb226afba8a52eafc718da06647c4431c
Olena Piankova <el.piankova@gmail.com>
2017-04-30 13:44:03 +0300

    Сделали проверку списков для теста удаления групп + Научили сравнивать на раверство и выводить представление Групп

--- tests/test_delete_group.py
+++ tests/test_delete_group.py
@@ -15,6 +15,8 @@ def test_delete_group(app, init_login, init_group):
     app.group.delete_by_number(0)
     assert "Group has been removed." in app.find_message()
     app.group.return_to_group_page()
-    # TODO: Deletion group in list
+    # Verifying Deletion group in list
     new_groups_list = app.group.get_list()
     assert len(old_groups_list)-1 == len(new_groups_list)
+    old_groups_list.pop(0)
+    assert old_groups_list == new_groups_list
