e646bc0f317e42fd750369c6a29cfffc3d073c7a
Olena Piankova <el.piankova@gmail.com>
2017-05-15 13:40:21 +0300

    Проверяем соединение с БД с помощью модуля PyMySQL

---
+++ check_db_connection.py
@@ -0,0 +1,21 @@
+import pymysql
+
+config = {
+    "host": "localhost",
+    "port": 8889,
+    "user": "root",
+    "password": "root",
+    "db": "test",
+    "charset": "utf8"
+}
+
+connection = pymysql.connect(**config)
+
+try:
+    with connection.cursor() as cursor:
+        sql = "SELECT * FROM group_list;"
+        cursor.execute(sql)
+        for row in cursor:
+            print(row)
+finally:
+    connection.close()
