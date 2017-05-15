68df4791674864f23f50a44766ace9cbac0f4802
Olena Piankova <el.piankova@gmail.com>
2017-05-15 13:15:24 +0300

    Считываем тестовые данные из JSON файла

--- data/test_groups.py
+++ data/test_groups.py
@@ -1,5 +1,6 @@
 import random
 import string
+import json
 from models.group import Group
 
 utf_symbols = ''.join([chr(l) for l in range(1, 0x10ffff) if chr(l).isprintable()])
@@ -12,17 +13,21 @@ def random_string(maxlen):
     symbols = string.ascii_letters + string.digits + " "*10 + string.punctuation + cyr_symbol_ru_uk
     return ''.join([random.choice(symbols) for _ in range(length)])
 
+with open("group_data.json", encoding='utf8') as f:
+    test_groups = [Group(**data) for data in json.load(f)]
 
-names = ['', 'hgvjhsdfcs', "123"]
-headers = ['', 'hgvjhsdfcs', "123"]
-footers = ['', 'hgvjhsdfcs', "123"]
+print(test_groups)
 
-test_groups = [
-    Group(name=name, header=header, footer=footer)
-    for name in names
-    for header in headers
-    for footer in footers
-] + [
-    Group(name=random_string(14), header=random_string(20), footer=random_string(50))
-    for _ in range(5)
-]
+# names = ['', 'hgvjhsdfcs', "123"]
+# headers = ['', 'hgvjhsdfcs', "123"]
+# footers = ['', 'hgvjhsdfcs', "123"]
+#
+# test_groups = [
+#     Group(name=name, header=header, footer=footer)
+#     for name in names
+#     for header in headers
+#     for footer in footers
+# ] + [
+#     Group(name=random_string(14), header=random_string(20), footer=random_string(50))
+#     for _ in range(5)
+# ]
