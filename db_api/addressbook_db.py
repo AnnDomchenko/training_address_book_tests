269c2ea42442af122d204cf95a5ff77657e8a4ec
Olena Piankova <el.piankova@gmail.com>
2017-05-15 14:14:07 +0300

    Добавлено получение списка Контактов и проверено в файле check_db_connection.py

--- db_api/addressbook_db.py
+++ db_api/addressbook_db.py
@@ -1,5 +1,6 @@
 import pymysql
 from models.group import Group
+from models.contact import Contact
 
 
 class AddressbookDB:
@@ -16,6 +17,17 @@ class AddressbookDB:
                 group_list.append(Group(id=row[0], name=row[1], header=row[2], footer=row[3]))
         self.connection.commit()
         return group_list
+
+    def get_contact_list(self):
+        with self.connection.cursor() as cursor:
+            sql = """SELECT id, firstname, middlename, lastname FROM addressbook 
+                     WHERE deprecated='0000-00-00 00:00:00';"""
+            cursor.execute(sql)
+            contact_list = []
+            for row in cursor:
+                contact_list.append(Contact(id=row[0], firstname=row[1], middlename=row[2], lastname=row[3]))
+        self.connection.commit()
+        return contact_list
 
     def destroy(self):
         self.connection.close()
