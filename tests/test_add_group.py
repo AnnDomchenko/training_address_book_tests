66102fdd069bdc2b57fe8d4daf38c96f7123c41e
Olena Piankova <el.piankova@gmail.com>
2017-05-24 00:19:53 +0300

    Для отчетов Allure (+ нужно установить pytest-allure-adaptor)

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -1,11 +1,17 @@
+import pytest
+
+
 def test_add_group(app, init_login, test_group, db):
-    app.group.open_group_page()
-    old_groups_list = db.get_group_list()
-    app.group.create(test_group)
-    assert "A new group has been entered into the address book." in app.find_message()
+    with pytest.allure.step("GIVEN a group list"):
+        old_groups_list = db.get_group_list()
+    with pytest.allure.step("WHEN I add the group {}".format(test_group)):
+        app.group.open_group_page()
+        app.group.create(test_group)
+    with pytest.allure.step("THEN the success message appears"):
+        assert "A new group has been entered into the address book." in app.find_message()
     app.group.return_to_group_page()
-    # Verifying new group presence in group list
-    new_groups_list = db.get_group_list()
-    assert len(old_groups_list) + 1 == len(new_groups_list)
-    old_groups_list.append(test_group)
-    assert sorted(old_groups_list) == sorted(new_groups_list)
+    with pytest.allure.step("THEN the new group list is equal to the old list with new added group"):
+        new_groups_list = db.get_group_list()
+        assert len(old_groups_list) + 1 == len(new_groups_list)
+        old_groups_list.append(test_group)
+        assert sorted(old_groups_list) == sorted(new_groups_list)
